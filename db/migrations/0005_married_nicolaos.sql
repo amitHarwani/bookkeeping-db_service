ecd69cff1c21bd3df547306d6e9200d92b258ff1
amitHarwani <harwania21@gmail.com>
2024-08-06 12:48:11 +0400

    Add Platform Features for sysadmin

---
+++ db/migrations/0005_married_nicolaos.sql
@@ -0,0 +1,11 @@
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'ADD_FEATURE';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'GET_ALL_FEATURES';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'UPDATE_FEATURE';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'ADD_COUNTRY';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'GET_ALL_COUNTRIES';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'UPDATE_COUNTRY';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'DELETE_COUNTRY';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'ADD_TAX_DETAILS';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'GET_ALL_TAX_DETAILS';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'UPDATE_TAX_DETAILS';--> statement-breakpoint
+ALTER TYPE "platformFeaturesEnum" ADD VALUE 'DELETE_TAX_DETAILS';
--- db/schema/platform_features.ts
+++ db/schema/platform_features.ts
@@ -1,21 +1,23 @@
 import { boolean, pgEnum, pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
+export const platformFeaturesEnum = pgEnum("platformFeaturesEnum", [
+    "ADD_FEATURE",
+    "GET_ALL_FEATURES",
+    "UPDATE_FEATURE",
 
-export const platformFeaturesEnum = pgEnum('platformFeaturesEnum', [
-    "ADD COUNTRY",
-    "GET COUNTRY",
-    "UPDATE COUNTRY",
-    "DELETE COUNTRY",
-    "ADD TAXDETAILS",
-    "GET TAXDETAILS",
-    "UPDATE TAXDETAILS",
-    "DELETE TAXDETAILS",
-])
+    "ADD_COUNTRY", 
+    "GET_ALL_COUNTRIES",
+    "UPDATE_COUNTRY",
+    "DELETE_COUNTRY",
 
+    "ADD_TAX_DETAILS",
+    "GET_ALL_TAX_DETAILS",
+    "UPDATE_TAX_DETAILS",
+    "DELETE_TAX_DETAILS",
+]);
 
-
-export const platformFeatures = pgTable('platform_features', {
-    featureId: serial('feature_id').primaryKey(),
-    featureName: platformFeaturesEnum('feature_name').unique().notNull(),
-    isEnabled: boolean('is_enabled').default(true)
-})
+export const platformFeatures = pgTable("platform_features", {
+    featureId: serial("feature_id").primaryKey(),
+    featureName: platformFeaturesEnum("feature_name").unique().notNull(),
+    isEnabled: boolean("is_enabled").default(true),
+});
