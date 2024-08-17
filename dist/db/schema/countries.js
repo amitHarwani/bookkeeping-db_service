ce612ad087b27c5486d7650f858c4f3eca9b09ba
amitHarwani <harwania21@gmail.com>
2024-08-17 19:46:10 +0400

    Add new build

--- dist/db/schema/countries.js
+++ dist/db/schema/countries.js
@@ -8,5 +8,6 @@ exports.countries = (0, pg_core_1.pgTable)('countries', {
     countryName: (0, pg_core_1.varchar)('country_name').unique().notNull(),
     phoneNumberCodes: (0, pg_core_1.varchar)('phone_number_codes').array().default((0, drizzle_orm_1.sql) `ARRAY[]::text[]`),
     currency: (0, pg_core_1.varchar)('currency').notNull(),
-    maxPhoneNumberDigits: (0, pg_core_1.integer)('max_phone_number_digits').notNull()
+    maxPhoneNumberDigits: (0, pg_core_1.integer)('max_phone_number_digits').notNull(),
+    timezone: (0, pg_core_1.varchar)("timezone")
 });
