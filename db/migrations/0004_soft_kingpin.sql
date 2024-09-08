83feeb7841872b9c366a2323976e0cbe75dbc0f9
amitHarwani <harwania21@gmail.com>
2024-09-08 15:38:54 +0400

    Add pricePerUnit and unitsSold to sale item profits

---
+++ db/migrations/0004_soft_kingpin.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "sale_item_profits" ADD COLUMN "price_per_unit" numeric NOT NULL;--> statement-breakpoint
+ALTER TABLE "sale_item_profits" ADD COLUMN "units_sold" numeric NOT NULL;
--- db/schema/sale_item_profits.ts
+++ db/schema/sale_item_profits.ts
@@ -8,6 +8,8 @@ export const saleItemProfits = pgTable("sale_item_profits", {
     itemId: integer("item_id").references(() => items.itemId),
     companyId: integer("company_id").references(() => companies.companyId),
     totalProfit: numeric("total_profit"),
+    pricePerUnit: numeric("price_per_unit").notNull(),
+    unitsSold: numeric("units_sold").notNull(),
     costOfItems: jsonb("cost_of_items").array(),
     purchaseIds: integer("purchase_ids").array().default(sql`ARRAY[]::integer[]`),
     remainingUnitsForProfitCalc: numeric("remaining_units_for_profit_calc"),
