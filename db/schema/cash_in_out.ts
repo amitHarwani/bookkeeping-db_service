e7e4a03743742e5cacb5385f5742920370e49d28
amitHarwani <harwania21@gmail.com>
2024-10-17 11:27:34 +0400

    Add saleReturnId in cashInOut table

--- db/schema/cash_in_out.ts
+++ db/schema/cash_in_out.ts
@@ -2,6 +2,7 @@ import { index, integer, numeric, pgTable, timestamp } from "drizzle-orm/pg-core
 import { companies } from "./companies";
 import { purchases } from "./purchases";
 import { sales } from "./sales";
+import { saleReturns } from "./sale_returns";
 
 
 export const cashInOut = pgTable("cash_in_out", {
@@ -10,7 +11,8 @@ export const cashInOut = pgTable("cash_in_out", {
     cashIn: numeric("cash_in").default("0").notNull(),
     cashOut: numeric("cash_out").default("0").notNull(),
     purchaseId: integer("purchase_id").references(() => purchases.purchaseId),
-    saleId: integer("sale_id").references(() => sales.saleId)
+    saleId: integer("sale_id").references(() => sales.saleId),
+    saleReturnId: integer("sale_return_id").references(() => saleReturns.saleReturnId)
 }, (table) => {
     return {
         transactionDateTimeIndex: index("transaction_date_time_index").on(table.transactionDateTime),
--- dist/db/schema/cash_in_out.js
+++ dist/db/schema/cash_in_out.js
@@ -5,13 +5,15 @@ const pg_core_1 = require("drizzle-orm/pg-core");
 const companies_1 = require("./companies");
 const purchases_1 = require("./purchases");
 const sales_1 = require("./sales");
+const sale_returns_1 = require("./sale_returns");
 exports.cashInOut = (0, pg_core_1.pgTable)("cash_in_out", {
     transactionDateTime: (0, pg_core_1.timestamp)("transaction_date_time", { withTimezone: false }).notNull(),
     companyId: (0, pg_core_1.integer)("company_id").references(() => companies_1.companies.companyId).notNull(),
     cashIn: (0, pg_core_1.numeric)("cash_in").default("0").notNull(),
     cashOut: (0, pg_core_1.numeric)("cash_out").default("0").notNull(),
     purchaseId: (0, pg_core_1.integer)("purchase_id").references(() => purchases_1.purchases.purchaseId),
-    saleId: (0, pg_core_1.integer)("sale_id").references(() => sales_1.sales.saleId)
+    saleId: (0, pg_core_1.integer)("sale_id").references(() => sales_1.sales.saleId),
+    saleReturnId: (0, pg_core_1.integer)("sale_return_id").references(() => sale_returns_1.saleReturns.saleReturnId)
 }, (table) => {
     return {
         transactionDateTimeIndex: (0, pg_core_1.index)("transaction_date_time_index").on(table.transactionDateTime),
